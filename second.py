2c935a5b1be94975fee5818a8ae45b442dd3bd27
Andreyc4d <sbtr@mail.ru>
2018-11-04 09:17:44 +0100

    second.py read 2 block of text in input.txt

--- second.py
+++ second.py
@@ -2,4 +2,8 @@ with open('input.txt', 'r') as f:
     n=int(f.readline())
     print('n='+str(n))
     for i in range(n):
-        print(f.readline()+ str(i))
+        print((f.readline().strip()))
+    n=int(f.readline())
+    print('n='+str(n))
+    for i in range(n):
+        print((f.readline().strip()))
