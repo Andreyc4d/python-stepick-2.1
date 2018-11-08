cbfe17a903e2e1e593a3b36caea2204990512542
Andreyc4d <sbtr@mail.ru>
2018-11-08 12:18:53 +0100

    I think all done

--- second.py
+++ second.py
@@ -1,10 +1,24 @@
-DescrClass = [] #description error class 2 dim list
+DescrClass = dict() #description error class 2 dim list
+AlreadySet = set()
+str1 = []
 with open('input.txt', 'r') as f:
     n=int(f.readline())
-    print('n='+str(n))
     for i in range(n):
-        print((f.readline().strip()))
+        str1 = f.readline().strip().split()
+        if len(str1) == 1:
+            DescrClass[str1[0]] = []
+        elif len(str1)>1:
+            DescrClass[str1[0]] = str1[2:]
     n=int(f.readline())
+    print(DescrClass)
+    print(len(DescrClass['OSError']))
     print('n='+str(n))
+    tmp1 = 0
     for i in range(n):
-        print((f.readline().strip()))
+        str2 = f.readline().strip()
+        for element in DescrClass[str2]:
+            if element in AlreadySet:
+                print(str2)
+                break
+        AlreadySet.add(str2)
+print(AlreadySet)
